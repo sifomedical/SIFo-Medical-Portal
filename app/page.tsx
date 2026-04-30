c49c5c802b13585e98e571d329f6cf8406a9613f
SIFo Medical <sifo.medical@gmail.com>
2026-04-30 14:18:25 +0200

    Minor: Format page.tsx for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export const dynamic = "force-dynamic";
 
 export default async function RootPage() {
   const session = await getServerSession();
-  if (!session) redirect("/login");
+
+  if (!session) {
+    redirect("/login");
+  }
+
   redirect("/dashboard");
 }
